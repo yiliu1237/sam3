3b16a1a4361374734162800e1f06733312be8ff1
yiliu1237 <150484524+yiliu1237@users.noreply.github.com>
2025-11-27 13:27:44 +0800

    fix init

--- sam3/__init__.py
+++ sam3/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved
 
-from .model_builder import build_sam3_image_model
+from .model_builder import build_sam3_image_model, build_sam3_video_predictor
 
 __version__ = "0.1.0"
 
-__all__ = ["build_sam3_image_model"]
+__all__ = ["build_sam3_image_model", "build_sam3_video_predictor"]
